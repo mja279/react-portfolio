1ecc93848818613ff4d7448004da024abe017cd4
Maria Jimena <mja279@georgetown.edu>
2020-11-01 18:13:49 -0500

    pushing updates with working code, files cleaned up

---
+++ src/components/pages/Home.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Home() {
+    return (
+        <div>
+            <h1>About Page</h1>
+            <p>
+            Born in Mar del Plata, a coastal town in the province of Buenos Aires, Argentina, my family migrated us to the United States when I was 6 years old. 
+            I was raised in the Washington-Metropolitan area, growing up in Montgomery County, Maryland. Currently, I reside in the Detroit-Metropolitan area where I am enrolled in a 
+            rigorous and fast-paced bootcamp focused on Full Stack Web Development. 
+            Since I was young, I've always enjoyed creating art and working on puzzles. Life has taught me to utilize my creativity as an advantage, allowing me to solve complex problems with 
+            unique solutions that are flexible and adaptable to any challenge presented. 
+            </p>
+        </div>
+    )
+}
---
+++ src/components/pages/Resume.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+export default function Resume() {
+    return (
+        <div>
+            <div class="resume-timeline-item-desc">
+                <ul class="a">
+                    <li>Develop project plans, custom workflows and processes for Discovery Services, Information Governance, and Structured Data Analytics projects with budgets up to $5 million</li>
+                    <li>Manage relationships with external and internal stakeholders, communicating deadlines, tasks, data delivery schedules, and custom reporting based on search results or budget analysis</li>
+                    <li>Utilize document review platform (Relativity) to create standard and custom indexes; search term development, iteration and optimization; use data elements to further cull data and validate sets for production; and, develop case management work product using Case Dynamics analysis tool</li>
+                    <li>Coordinate with engineering team to intake data, define processes for QC and QA resolutions and ensure a high quality of work product</li>
+                    <li>Perform basic legal and factual research, maintaining litigation case tracking</li>
+                    <li>Collaborate with cross-functional teams to intake data, define processes for QC/QA and ensure a high quality of work product</li>
+                </ul>
+                <h4 class="resume-timeline-item-desc-heading font-weight-bold">Technologies used:</h4>
+                <ul class="list-inline">
+                    <li class="list-inline-item"><span class="badge badge-primary badge-pill">Relativity</span></li>
+                    <li class="list-inline-item"><span class="badge badge-primary badge-pill">Brainspace</span></li>
+                    <li class="list-inline-item"><span class="badge badge-primary badge-pill">Bloomberg</span></li>
+                    <li class="list-inline-item"><span class="badge badge-primary badge-pill">Excel</span></li>
+                    <li class="list-inline-item"><span class="badge badge-primary badge-pill">MySQL</span></li>
+                </ul>
+            </div>
+        </div>
+    )
+}
